50ef810f3940ea7587841993d7628204e272f7ce
Matt Proud <laidbackware.git@gmail.com>
2023-07-27 11:56:25 +0100

    got it working!

--- cf_asg_analyser.py
+++ cf_asg_analyser.py
@@ -1,19 +1,39 @@
 '''Looks for ASG optimisations'''
-import json, os, sys
+import json, sys
 
 def get_rule_string_list(rules):
-    rule_list = []
-    for rule in rules:
-      rule_list.append(f"{rule['destination']}_{rule['protocol']}_{rule['ports']}")
-    return rule_list
+  rule_list = []
+  for rule in rules:
+    rule_list.append(f"{rule['destination']}_{rule['protocol']}_{rule['ports']}")
+  return rule_list
 
-def main():
-  
-  with open('dummy.json') as f:
-    asg_data = json.load(f)
-    print(asg_data)
+def main(file_name):
+  '''Main function'''
+  with open(file_name, encoding="utf-8") as open_file:
+    asg_data = json.load(open_file)
 
-  parse_asgs(asg_data)
+  covered_by_defaults, org_data = parse_asgs(asg_data)
+
+  org_common_saving = look_for_common_org_rules(org_data)
+
+  rules_processed = 0
+  for asg in asg_data:
+    rules_processed += len(asg["rules"])
+
+  print(f"Processed {len(asg_data)} ASGs")
+  print(f"Processed {rules_processed} rules")
+  print(f"Number of rules covered by defaults: {len(covered_by_defaults)}")
+  print(f"Number of rules that could be covered by common org ASG: {org_common_saving}")
+
+def look_for_common_org_rules(org_data):
+  org_common_saving = 0
+
+  for _, org_details in org_data.items():
+    for _, rule_spaces in org_details["rules"].items():
+      if org_details["space_count"] > 1 and len(rule_spaces) == org_details["space_count"]:
+        org_common_saving += org_details["space_count"]-1
+
+  return org_common_saving
 
 def iterate_dict_value(lookup_dict, key):
   if not key in lookup_dict:
@@ -22,14 +42,16 @@ def iterate_dict_value(lookup_dict, key):
   lookup_dict[key] = lookup_dict[key] + 1
 
 def extract_org_space_numbers(asg_data):
+  '''Returns a dict, with org name as key and space_count/spaces as subkeys '''
   org_space_numbers = {}
   for asg in asg_data:
     for org_space_joined in asg["spaces"]:
-      org_name, space_name = org_space_joined.split("_")
+      org_name, space_name = org_space_joined.split("_")[0], org_space_joined.split("_")[1]
       if not org_name in org_space_numbers:
         org_space_numbers[org_name] = {
           "space_count": 1,
-          "spaces": {space_name}
+          "spaces": {space_name},
+          "rules": {}
         }
         continue
       if not space_name in org_space_numbers[org_name]["spaces"]:
@@ -37,49 +59,44 @@ def extract_org_space_numbers(asg_data):
         org_space_numbers[org_name]["spaces"].add(space_name)
   return org_space_numbers
 
+def assign_rule_org_mapping(org_data, asg, rule_string):
+  for org_space_name  in asg["spaces"]:
+    org_name, space_name = org_space_name.split("_")[0], org_space_name.split("_")[1]
+
+    if rule_string not in  org_data[org_name]["rules"]:
+      org_data[org_name]["rules"][rule_string] = {space_name}
+      continue
+    org_data[org_name]["rules"][rule_string].add(space_name)
+
 def parse_asgs(asg_data):
   # Tests
   # [x] destination is covered by the default ASG
-  # [ ] destination appears all spaces in the same org
+  # [x] destination appears all spaces in the same org
+  # [ ] destination is mapped to same space via another ASG
   # [ ] destination appears in a large number of orgs
-  # [ ] destination duplicated between ASGs that cover the same org
-
-  # org_destinations_set = {}
+  # [ ] check for duplicate rules inside an ASG
+  # [ ] check for rules that could be combined due to a shared target and protocol
+  # [ ] asg is applied to to many spaces for NSX-T
 
   default_rules = get_rule_string_list(asg_data[0]["rules"])
-
-  org_space_numbers = extract_org_space_numbers(asg_data)
-
+  org_data = extract_org_space_numbers(asg_data)
   covered_by_defaults = []
-  org_rule_relationship = {}
 
   for asg in asg_data:
     # Skip default ASG
-    if asg["asg_name"] == "default_security_group":
+    if asg["asg_name"] == "default_security_group" or "" not in asg["asg_name"]:
       continue
 
     for rule in asg["rules"]:
       rule_string = f"{rule['destination']}_{rule['protocol']}_{rule['ports']}"
 
       if rule_string in default_rules:
-        covered_by_defaults.append(rule.update({"org_space": asg["spaces"]}))
+        covered_by_defaults.append(rule.update({"org_space": asg["spaces"], "asg_name": asg["asg_name"]}))
 
-      
-      iterate_dict_value(org_rule_relationship, rule_string)
-      # if org_rule_count[rule_string]
-      # rule_set.add(f"{rule['destination']}_{rule['protocol']}_{rule['ports']}")
-    
-    
-    for org_rule_mapping, org_rule_count in org_rule_relationship.items():
-      org_name = org_rule_mapping.split("")[0]
-
-
-  return {
-    "covered_by_defaults": covered_by_defaults
-  }
-  # print(f"Rules covered by default: {len(covered_by_defaults)}")
+      assign_rule_org_mapping(org_data, asg, rule_string)
+  return covered_by_defaults, org_data
 
 if __name__ == "__main__":
-    # TODO test for user input!!!
-    file_name = sys.argv[1]
-    main(file_name)
+  # TODO test for user input!!!
+  FILE_NAME = sys.argv[1]
+  main(FILE_NAME)
--- cf_asg_analyser_test.py
+++ cf_asg_analyser_test.py
@@ -1,7 +1,5 @@
 '''Test cf_asg_optimiser'''
 import os
-import tempfile
-import pytest
 import json
 
 import cf_asg_analyser as cfa
@@ -10,17 +8,54 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 
 DUMMY_FILE = os.path.join(base_dir, "test", "dummy.json")
 
-with open(DUMMY_FILE) as f:
-    asg_data = json.load(f)
-    print(asg_data)
+asg = {
+    "spaces": [
+      "o1_s1",
+      "o1_s2",
+      "o1_s3",
+      "o2_s4",
+    ]
+  }
+
+# org_data = {
+#    "o1": {
+
+#    }
+# }
+
+with open(DUMMY_FILE, encoding="utf-8") as open_file:
+  asg_data = json.load(open_file)
+  print(asg_data)
 
 def test_parse_args():
   '''Ensure runs e2e without error'''
-  resp = cfa.parse_asgs(asg_data)
+  covered_by_defaults, _ = cfa.parse_asgs(asg_data)
 
-  assert len(resp["covered_by_defaults"]) == 1, "Detected too many rules covered by default"
+  assert len(covered_by_defaults) == 1, "Detected too many rules covered by default"
 
 def test_extract_org_space_numbers():
   resp = cfa.extract_org_space_numbers(asg_data)
 
   assert "o1" in resp
+
+# def test_extract_org_space_dict():
+#    resp = cfa.extract_org_space_dict(asg_data[5])
+#    assert len(resp) == 2
+#    assert type(resp["o3"]) == list and len(resp["o3"]) == 3
+#    assert resp["o4"] == ["s4"]
+
+def test_assign_rule_org_mapping():
+  rule_string = "destination_protocol_ports"
+  org_data = {
+    "o3": {"rules": {}},
+    "o4": {"rules": {}}
+  }
+  cfa.assign_rule_org_mapping(org_data, asg_data[5], f"{rule_string}_1")
+  cfa.assign_rule_org_mapping(org_data, asg_data[5], f"{rule_string}_2")
+
+  assert len(org_data["o3"]["rules"]) == 2
+
+def test_look_for_common_org_rules():
+  _, org_data = cfa.parse_asgs(asg_data)
+  org_common_saving = cfa.look_for_common_org_rules(org_data)
+  assert org_common_saving == 8
